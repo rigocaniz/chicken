71bd711c51675e959ecf07aab61b504ba4463ea7
rigocaniz <rigocaniz@gmail.com>
2017-07-30 16:50:20 -0600

    Menus, Orden, Selección

--- js/crtlOrden.js
+++ js/crtlOrden.js
@@ -1,3 +1,23 @@
-app.controller('crtlOrden', function( $scope, $http ){
-	
+app.controller('crtlOrden', function( $scope, $http, $timeout, $modal ){
+	$scope.lstMenu = [
+		{ idMenu : 1, menu : 'Pollo Frito', img : 'pollo.png' },
+		{ idMenu : 2, menu : 'Papas Fritas', img : 'papas.png' },
+		{ idMenu : 3, menu : 'Pizza', img : 'pizza.png' },
+		{ idMenu : 4, menu : 'Hamburguesa', img : 'hamburguesa.png' },
+	];
+
+	$scope.dialOrden        = $modal({scope: $scope,template:'dial.orden.html', show: false});
+	$scope.dialMenuCantidad = $modal({scope: $scope,template:'dial.menu-cantidad.html', show: false});
+
+	$timeout(function () {
+		$scope.dialOrden.show();
+	});
+
+	$scope.menuActual = {};
+	$scope.seleccionarMenu = function ( menu ) {
+		$scope.menuActual          = angular.copy( menu );
+		$scope.menuActual.cantidad = 1;
+		$scope.dialOrden.hide();
+		$scope.dialMenuCantidad.show();
+	};
 });
--- view/orden.php
+++ view/orden.php
@@ -62,3 +62,79 @@
 		</div>
 	</div>
 </div>
+
+<!-- Administrar Orden -->
+<script type="text/ng-template" id="dial.orden.html">
+    <div class="modal bs-example-modal-lg" tabindex="-1" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content panel-primary">
+                <div class="modal-header panel-heading">
+                	<button type="button" class="close" ng-click="$hide()">&times;</button>
+                    Seleccione Menú
+                </div>
+                <div class="modal-body">
+                	<div class="row">
+                		<div class="col-md-3 col-sm-4 col-xs-6 text-center" ng-repeat="item in lstMenu">
+                			<button type="button" class="menu-btn" ng-click="seleccionarMenu( item )">
+	                			<img ng-src="img/{{item.img}}">
+	                			<span>{{item.menu}}</span>
+                			</button>
+                		</div>
+                   </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" ng-click="$hide()">
+                        <span class="glyphicon glyphicon-log-out"></span>
+                        <b>Salir</b>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</script> 
+
+<!-- Menú Seleccionado-Cantidad -->
+<script type="text/ng-template" id="dial.menu-cantidad.html">
+    <div class="modal bs-example-modal-lg" tabindex="-1" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content panel-info">
+                <div class="modal-header panel-heading">
+                	<button type="button" class="close" ng-click="$hide()">&times;</button>
+                    <b>Ingrese Cantidad Menú</b>
+                </div>
+                <div class="modal-body">
+                	<div class="row">
+                		<div class="col-xs-6 text-center">
+                			<img ng-src="img/{{menuActual.img}}">
+                			<h4>{{menuActual.menu}}</h4>
+                		</div>
+                		<div class="col-xs-6">
+                			<div class="col-xs-12">
+	                			<label>Cantidad</label>
+	                			<input type="number" class="form-control input-lg" ng-model="menuActual.cantidad" min="1" style="font-weight:bold;">
+                			</div>
+                			<div class="col-xs-12" style="margin-top:5px">
+                				<button type="button" class="btn btn-sm btn-default" ng-click="menuActual.cantidad=menuActual.cantidad-1" ng-disabled="!(menuActual.cantidad>1)">
+                					<span class="glyphicon glyphicon-minus"></span>
+                				</button>
+                				<button type="button" class="btn btn-sm btn-primary" ng-click="menuActual.cantidad=menuActual.cantidad+1">
+                					<span class="glyphicon glyphicon-plus"></span>
+                				</button>
+                			</div>
+                		</div>
+                   </div>
+                </div>
+                <div class="modal-footer">
+                	<button type="button" class="btn btn-success" ng-click="agregarAPedido()">
+                        <span class="glyphicon glyphicon-plus-sign"></span>
+                        <b>Agregar a Orden</b>
+                    </button>
+                    <button type="button" class="btn btn-default" ng-click="$hide()">
+                        <span class="glyphicon glyphicon-remove"></span>
+                        <b>Cancelar</b>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</script> 
