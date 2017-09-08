d0796936bc1128d5926f309e7acae2584b7b5997
agapo88 <aga.xuruc88@gmail.com>
2017-09-07 23:19:45 -0600

    Cambio Encabezado FActura

--- print.php
+++ print.php
@@ -5,35 +5,44 @@ include 'class/facturar.class.php';
 
 $factura = new Factura();
 
-$idFactura        = 7;
+$idFactura        = 10;
 
 $principalFactura = (object)$factura->lstFacturas( $idFactura )[ 0 ];
 $detalleFactura   = $factura->detalleOrdenFactura( $idFactura );
+/*
 echo "<pre>";
 print_r( $principalFactura );
 echo "</pre>";
-/*
 */
 ?>
+<style type="text/css">
+	table {
+		border-collapse:collapse
+	}
+	td {
+		border:1px solid black;
+	}
+</style>
 
-
-<table border="1">
+<table border="0" cellpadding="2">
 	<tbody>
 		<tr>
-			<td>Nombre</td>
-			<td><?= $principalFactura->nombre; ?></td>
-			<td>NIT</td>
+			<td><b>NIT:</b></td>
 			<td><?= $principalFactura->idFactura; ?></td>
 		</tr>
 		<tr>
-			<td>Dirección</td>
+			<td><b>Nombre</b>:</td>
+			<td><?= $principalFactura->nombre; ?></td>
+		</tr>
+		<tr>
+			<td><b>Dirección</b>:</td>
 			<td colspan="4"><?= $principalFactura->direccion; ?></td>
 		</tr>
 	</tbody>
 </table>
 <br>
 
-<table border="1">
+<table border="1" cellpadding="4">
 	<thead>
 		<tr>
 			<th>No.</th>
