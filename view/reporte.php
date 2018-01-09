e0af29da8c23a8bc437829d78b5d7e755651edc6
agapo88 <aga.xuruc88@gmail.com>
2018-01-08 21:04:22 -0600

    Solo Maquetación Reporte

--- view/reporte.php
+++ view/reporte.php
@@ -17,6 +17,11 @@
 	        </div>
 			<!-- TABS -->
 			<ul class="nav nav-tabs tabs-title" role="tablist">
+				<li role="presentation">
+					<a href="#/">
+						<span class="glyphicon glyphicon-home"></span>
+					</a>
+				</li>
 				<li role="presentation" ng-class="{'active' : reporteMenu==1}" ng-click="reporteMenu=1;">
 					<a href="" role="tab" data-toggle="tab">
 						VENTAS
@@ -42,16 +47,87 @@
 			<div class="tab-content">
 				<!--  VENTAS -->
 				<div role="tabpanel" class="tab-pane" ng-class="{'active' : reporteMenu==1}" ng-show="reporteMenu==1">
-					
+					<form class="form-horizontal">
+						<div class="form-group">
+						    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+						    	<label><i class="fa fa-calendar"></i> DE FECHA:</label>
+						      	<input class="form-control" ng-model="fromDate" data-max-date="{{ untilDate }}" placeholder="dd/mm/aaaa" bs-datepicker type="text">
+						    </div>
+						    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+						    	<label><i class="fa fa-calendar"></i> A FECHA:</label>
+						      	<input class="form-control" ng-model="untilDate" data-min-date="{{ fromDate }}" placeholder="dd/mm/aaaa" bs-datepicker type="text">
+						    </div>
+						    <div>
+						    	<br>
+						    	<button type="button" class="btn btn-sm btn-danger">
+						    		GENERAR REPORTE <span class="glyphicon glyphicon-download-alt"></span>
+						    	</button>
+						    </div>
+						</div>
+					</form>
 				</div>
 				<!-- PEDIDOS CANCELADO-->
 				<div role="tabpanel" class="tab-pane" ng-class="{'active' : reporteMenu==2}" ng-show="reporteMenu==2">
-					
+					<form class="form-horizontal">
+						<div class="form-group">
+						    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+						    	<label><i class="fa fa-calendar"></i> DE FECHA:</label>
+						      	<input class="form-control" ng-model="fromDate" data-max-date="{{ untilDate }}" placeholder="dd/mm/aaaa" bs-datepicker type="text">
+						    </div>
+						    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+						    	<label><i class="fa fa-calendar"></i> A FECHA:</label>
+						      	<input class="form-control" ng-model="untilDate" data-min-date="{{ fromDate }}" placeholder="dd/mm/aaaa" bs-datepicker type="text">
+						    </div>
+						    <div>
+						    	<br>
+						    	<button type="button" class="btn btn-sm btn-danger">
+						    		GENERAR REPORTE <span class="glyphicon glyphicon-download-alt"></span>
+						    	</button>
+						    </div>
+						</div>
+					</form>
 				</div>
 				<!-- INGRESOS -->
 				<div  role="tabpanel" class="tab-pane" ng-class="{'active' : reporteMenu==3}" ng-show="reporteMenu==3">
+					<form class="form-horizontal">
+						<div class="form-group">
+						    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+						    	<label><i class="fa fa-calendar"></i> DE FECHA:</label>
+						      	<input class="form-control" ng-model="fromDate" data-max-date="{{ untilDate }}" placeholder="dd/mm/aaaa" bs-datepicker type="text">
+						    </div>
+						    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+						    	<label><i class="fa fa-calendar"></i> A FECHA:</label>
+						      	<input class="form-control" ng-model="untilDate" data-min-date="{{ fromDate }}" placeholder="dd/mm/aaaa" bs-datepicker type="text">
+						    </div>
+						    <div>
+						    	<br>
+						    	<button type="button" class="btn btn-sm btn-danger">
+						    		GENERAR REPORTE <span class="glyphicon glyphicon-download-alt"></span>
+						    	</button>
+						    </div>
+						</div>
+					</form>
+				</div>
 				<!-- INVENTARIO DE PRODUCTOS -->
 				<div  role="tabpanel" class="tab-pane" ng-class="{'active' : reporteMenu==4}" ng-show="reporteMenu==4">
+					<form class="form-horizontal">
+						<div class="form-group">
+						    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+						    	<label><i class="fa fa-calendar"></i> DE FECHA:</label>
+						      	<input class="form-control" ng-model="fromDate" data-max-date="{{ untilDate }}" placeholder="dd/mm/aaaa" bs-datepicker type="text">
+						    </div>
+						    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3">
+						    	<label><i class="fa fa-calendar"></i> A FECHA:</label>
+						      	<input class="form-control" ng-model="untilDate" data-min-date="{{ fromDate }}" placeholder="dd/mm/aaaa" bs-datepicker type="text">
+						    </div>
+						    <div>
+						    	<br>
+						    	<button type="button" class="btn btn-sm btn-danger">
+						    		GENERAR REPORTE <span class="glyphicon glyphicon-download-alt"></span>
+						    	</button>
+						    </div>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
