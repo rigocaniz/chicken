05b1596420f05ebae35d2b35fa3c04546530134e
rigocaniz <rigocaniz@gmail.com>
2018-03-15 05:21:16 -0600

    Metodo y consulta de Ventas por Rango de Fecha

--- class/orden.class.php
+++ class/orden.class.php
@@ -512,17 +512,18 @@ class Orden
 		$idOrdenCliente = (int)$idOrdenCliente;
 
 		// SI EL ESTADO ES DIFERENTE A PENDIENTE Y LIMITE ES MAYOR A CERO
- 		if ( $limite > 0 AND $idEstadoOrden > 2 )
- 		{
-			$where = " WHERE idEstadoOrden = " . $idEstadoOrden;
+ 		if ( $limite > 0 AND $idEstadoOrden > 4 )
  			$limit = " LIMIT " . $limite;
- 		}
 
  		if ( $idOrdenCliente > 0 )
 			$where = " WHERE idOrdenCliente = " . $idOrdenCliente;
 
 		else if ( $idEstadoOrden == 1 OR $idEstadoOrden == 2 )
 			$where = " WHERE ( idEstadoOrden = 1 OR idEstadoOrden = 2 ) ";
+
+		else if ( $idEstadoOrden > 2 )
+			$where = " WHERE idEstadoOrden = " . $idEstadoOrden;
+
 
  		$sql = "SELECT 
 					idOrdenCliente, numeroTicket, usuarioResponsable, idEstadoOrden, estadoOrden, fechaRegistro, numMenu
--- class/reporte.class.php
+++ class/reporte.class.php
@@ -29,123 +29,56 @@ class Reporte
 
 
  	// OBTENER VENTAS POR FECHA
- 	public function getVentasFecha( $filtro, $deFecha, $aFecha )
+ 	public function getVentasFecha( $deFecha, $paraFecha )
  	{
  		$ventas = new stdClass();
- 		$ventas->total         = 0;
- 		$ventas->detalleVentas = [];
- 		$ventas->encontrado    = FALSE;
+		$ventas->totalVentas   = 0;
+		$ventas->totalEstimado = 0;
+		$ventas->detalleVentas = [];
+		$ventas->encontrado    = FALSE;
 
- 		$sql = "SELECT * 
-					FROM vstFactura AS vF
-						JOIN vstDetalleOrdenFactura AS vOf
-							ON vF.idFactura = vOf.idFactura
-					WHERE vF.fechaFactura AND vF.idEstadoFactura
-					BETWEEN '{$deFecha}' AND '{$aFecha}';";
+ 		$sql = "SELECT
+					DATE_FORMAT( f.fechaFactura, '%d/%m/%Y' )AS 'fechaFactura',
+				    IFNULL( m.codigo, IFNULL( c.codigo, 'N/A' ) )AS 'codigoMenu',
+				    IFNULL( m.menu, IFNULL( c.combo, mp.descripcion ) )AS 'descripcionOrden',
+				    COUNT( dof.idFactura )AS 'cantidad',
+				    SUM( dof.precioMenu )AS 'totalEstimado',
+				    SUM( ( dof.precioMenu - dof.descuento ) )AS 'totalVenta'
+				FROM factura AS f
+					JOIN detalleOrdenFactura AS dof
+						ON f.idFactura= dof.idFactura
+				        
+					LEFT JOIN detalleOrdenMenu AS dom
+						ON dom.idDetalleOrdenMenu = dof.idDetalleOrdenMenu
+					
+				    LEFT JOIN menu AS m
+						ON m.idMenu = dom.idMenu
+					
+				    LEFT JOIN detalleOrdenCombo AS doc
+						ON doc.idDetalleOrdenCombo = dof.idDetalleOrdenCombo
+					
+				    LEFT JOIN combo AS c
+						ON c.idCombo = doc.idCombo
+				        
+					LEFT JOIN menuPersonalizado AS mp
+						ON mp.idMenuPersonalizado = dof.idMenuPersonalizado
+				WHERE f.idEstadoFactura = 1 
+					AND ( f.fechaFactura BETWEEN '{$deFecha}' AND '{$paraFecha}' )
+				GROUP BY f.fechaFactura, codigoMenu, descripcionOrden
+				ORDER BY f.fechaFactura ASC ";
 
 		if( $rs = $this->con->query( $sql ) )
 		{
 			if( $rs->num_rows )
 			{
 				$ventas->encontrado = TRUE;
-	 			while( $row = $rs->fetch_object() )
-	 			{
-	 				$iFactura = -1;
-
-	 				if( $filtro == 'factura' )
-	 				{
-	 					foreach ( $ventas->detalleVentas AS $ixFactura => $factura ) {
-	 						if( $factura->idFactura == $row->idFactura )
-	 						{
-	 							$iFactura = $ixFactura;
-	 							break;
-	 						}
-	 					}
-	 				}
-
-
-	 				if( $iFactura == -1 )
-	 				{
-	 					$iFactura = count( $ventas );
-	 					$ventas->detalleVentas[ $iFactura ] = 
-								(object)array(
-									'idFactura'      => $row->idFactura,
-									'total'          => $row->total,
-									'detalleFactura' => array()
-			 					);
-	 				}
-
-
-	 				if( $filtro == 'factura' )
-	 					$ventas->detalleVentas[ $iFactura ]->detalleFactura[] = $row;
-
-/*
-	 				$row->idCombo             = (int)$row->idCombo?:NULL;
-					$row->idMenu              = (int)$row->idMenu?:NULL;
-					$row->idDetalleOrdenCombo = (int)$row->idDetalleOrdenCombo;
-					$row->perteneceCombo      = (int)$row->perteneceCombo;
-					
-					// SI PERTENECE A COMBO
-					if ( $row->perteneceCombo ) {
-						$row->idMenu             = 0;
-						$row->idDetalleOrdenMenu = 0;
-					}
-					else{
-						$row->idCombo             = 0;
-						$row->idDetalleOrdenCombo = 0;
-					}
-
-	 				$index = -1;
-
-					// REVISA SI YA EXISTE MENU
-					foreach ( $ventas->detalleVentas as $ix => $item ):
-						if (
-							$row->idTipoServicio == $item->idTipoServicio
-							AND ( 
-								( $row->perteneceCombo AND $row->perteneceCombo == $item->esCombo AND $row->idCombo == $item->idCombo )
-								OR 
-								( !$row->perteneceCombo AND $row->perteneceCombo == $item->esCombo AND $row->idMenu == $item->idMenu )
-							)
-						){
-							$index = $ix;
-							break;
-						}
-					endforeach;
-
-					// SI NO EXISTE EN LISTADO => AGREGA UNA NUEVA ORDEN
-					if ( $index == -1 )
-					{
-						$index = count( $ventas->detalleVentas );
-						$ventas->detalleVentas[ $index ] = 				
-							(object)
-								array(
-									'idCombo'             => $row->idCombo,
-									'idMenu'              => $row->idMenu,
-									'esCombo'             => $row->perteneceCombo,
-									'cantidad'            => 0,
-									'subTotal' 			  => 0,
-									'descuento'           => (double)$row->descuento,
-									'precioMenu'          => (double)$row->precioMenu,
-									'precioReal'          => $row->precioReal,
-									'descripcion'         => ( $row->perteneceCombo ? $row->combo : $row->menu ),
-									'idTipoServicio'      => $row->idTipoServicio,
-									'tipoServicio'        => $row->tipoServicio,
-									'comentario'          => $row->comentario,
-									'idDetalleOrdenMenu'  => $row->idDetalleOrdenMenu,
-									'idDetalleOrdenCombo' => $row->idDetalleOrdenCombo,
-								);
-					}
-
-					$ventas->detalleVentas[ $index ]->cantidad += 1;
-					$ventas->detalleVentas[ $index ]->subTotal = ( $ventas->detalleVentas[ $index ]->cantidad * $row->precioReal );
-					// SUMA EL TOTAL DE LA ORDEN
-					$ventas->total += (double)$row->precioMenu;
- 		*/
-	 			}
-			}
-			else{
-				$ventas->mensaje   = 'No se encontraron resultados';
-				$ventas->respuesta = $this->respuesta;
+	 			while( $row = $rs->fetch_object() ):
+					$row->totalEstimado      = (double)$row->totalEstimado;
+					$row->totalVenta         = (double)$row->totalVenta;
+					$ventas->totalEstimado   += $row->totalEstimado;
+					$ventas->totalVentas     += $row->totalVenta;
+					$ventas->detalleVentas[] = $row;
+				endwhile;
 			}
  		}
 
